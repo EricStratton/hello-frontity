574991e7242395f07392ad606a63d19eed6283b7
EricStratton <strattonericj@gmail.com>
2021-10-17 13:25:54 -0700

    add basic components

--- packages/my-first-theme/src/components/index.js
+++ packages/my-first-theme/src/components/index.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import { connect } from 'frontity';
+import Link from '@frontity/components/link';
+import Switch from '@frontity/components/switch';
 
-const Root = () => {
+import List from './list';
+import Post from './post';
+import Page from './page';
+
+const Root = ({ state }) => {
+  const data = state.source.get(state.router.link)
+
   return (
     <>
-      <h1>Hello Frontity</h1>
+      <h1>Frontity Workshop</h1>
+      <p>Current URL: {state.router.link}</p>
+      <nav>
+        <Link link='/'>Home</Link>
+        <br />
+        <Link link='/page/2'>More posts</Link>
+        <br />
+        <Link link='/about-us'>About Us</Link>
+      </nav>
+      <hr />
+      <main>
+        <Switch>
+          <List when={data.isArchive} />
+          <Post when={data.isPost}>This is a post</Post>
+          <Page when={data.isPage}>This is a page</Page>
+        </Switch>
+      </main>
     </>
   );
 };
 
-export default Root;
+export default connect(Root);
